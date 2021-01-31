5590ea31e6e12fcb9cc375ed9ba9aff3d1346bc7
chentao <3413218@gmail.com>
2021-01-31 22:44:41 +0800

    feat: 累计的提交

--- packages/website/src/App.vue
+++ packages/website/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <input type='text' @keyup="fetchPackageList">
+  <input type='text' @keyup="fetchPackageList" v-model="keyword">
   <package-list :list="list" @view-detail="fetchPackage" v-if="!detail"></package-list>
+  <button v-if="detail" @click="handleReturn">返回</button>
   <package-detail v-bind="detail" v-if="detail"></package-detail>
 </template>
 
@@ -14,7 +15,8 @@ export default {
   data(){
     return {
       list:[],
-      detail:null
+      detail:null,
+      keyword:""
     }
   },
   components: {
@@ -26,12 +28,20 @@ export default {
          this.detail = null;
          const q = ev.target.value;
          this.getSuggestion(q, (error, data) => {
-           this.list = data;
+           if(!error){
+             this.list = data;
+           }
+           
          });
     },
     async fetchPackage(packageName){
           const response = await axios.get(`http://127.0.0.1:3000/npmjs/package/${packageName}/document`);
           this.detail = response.data;
+    },
+    handleReturn(){
+      this.detail = null
+      this.keyword = "";
+      this.list = []
     }
   },
   created(){
