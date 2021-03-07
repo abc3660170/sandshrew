26c5cede6217c68f4b3bdfa831105d225e662965
chentao <3413218@gmail.com>
2021-03-07 21:56:34 +0800

    feat: 累计的更新

--- api/npmjs.js
+++ api/npmjs.js
@@ -3,13 +3,23 @@ var express = require("express");
 var router = express.Router();
 var makeDB = require('../model/makeDB');
 var app = express();
-
+var { getFrontType } = require('../utils/utils');
+var { getSuggestions, getPackageDocument } = require('../model/verdaccio')
+const frontType = getFrontType();
 /* GET users listing. */
 router.get('/suggestions', async function(req, res, next) {
     try {
         const keyword = req.query.q;
-        const response = await axios.get(`https://www.npmjs.com/search/suggestions?q=${keyword}`);
-        res.json(response.data);
+        let result;
+        if(frontType === 'npmjs'){
+            const response = await axios.get(`https://www.npmjs.com/search/suggestions?q=${keyword}`);
+            result = response.data;
+        } else if (frontType === 'verdaccio'){
+            result = await getSuggestions(keyword);
+        } else {
+            throw new Error('frontType is not in [\'npmjs, verdaccio\']')
+        }
+        res.json(result);
     } catch (error) {
         next(error)
     }  
@@ -18,8 +28,16 @@ router.get('/suggestions', async function(req, res, next) {
 router.get('/package/:name/document', async function(req, res, next) {
     try {
         const packageName  = req.params.name;
-        const response = await axios.get(`https://registry.npmjs.org/${packageName}`);
-        res.json(response.data);
+        let result;
+        if(frontType === 'npmjs'){
+            const response = await axios.get(`https://registry.npmjs.org/${packageName}`);
+            result = response.data;
+        } else if (frontType === 'verdaccio'){
+            result = await getPackageDocument(packageName);
+        } else {
+            throw new Error('frontType is not in [\'npmjs, verdaccio\']')
+        }
+        res.json(result);
     } catch (error) {
         next(error)
     }  
