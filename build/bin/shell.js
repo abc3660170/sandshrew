40898b0cbc836c59c73500390b486674ab634513
tchen <tchen@njpt.com>
2021-03-22 11:13:40 +0800

    删除了不必要的文件

--- build/bin/shell.js
+++
@@ -1,9 +0,0 @@
-const command = process.argv[2];
-
-var localNpm = require('../local-npm');
-if(command === 'start'){
-    var app = localNpm(command);
-    setTimeout(() => {
-        app.shutdown();
-    },4000)
-}
