fa291c62dade93e637caccfadcd5c793a6b41c21
tchen <tchen@njpt.com>
2021-03-23 09:25:29 +0800

    feat: 内网适配调整

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/dist')));
-if(mirrorStorage){
-  console.log(mirrorPath)
+if(mirrorPath && !/^http/.test(mirrorPath)){
   app.use(mirrorPath, express.static(mirrorStorage));
 }
 
