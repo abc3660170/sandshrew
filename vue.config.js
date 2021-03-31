f53face40d6fe1ed3a8ce96daf102cd5866f72c5
tchen <tchen@njpt.com>
2021-03-31 09:56:02 +0800

    feat: 更新了titile

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
       filename: "index.html",
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      title: "Sandshrew",
+      title: "Sandshrew-NPM同步工具",
       // 在这个页面中包含的块，默认情况下会包含
       // 提取出来的通用 chunk 和 vendor chunk。
       chunks: ["chunk-vendors", "chunk-common", "index"],
