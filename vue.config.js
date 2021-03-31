3d866cc9834aa4b9c6986171c96bf1627e16cfb9
tchen <tchen@njpt.com>
2021-03-31 09:49:28 +0800

    fix: 修改了模板路径

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,27 @@
 var { getAppConfig } = require("./utils/utils");
 module.exports = {
-    chainWebpack: config => {
-        config
-            .plugin('define')
-            .tap(args => {
-                args[0]['process.env'] = {
-                    ...args[0],
-                    'VUE_APP_PORT': JSON.stringify(getAppConfig().port)
-                }
-                return args
-            })
+  chainWebpack: (config) => {
+    config.plugin("define").tap((args) => {
+      args[0]["process.env"] = {
+        ...args[0],
+        VUE_APP_PORT: JSON.stringify(getAppConfig().port),
+      };
+      return args;
+    });
+  },
+  pages: {
+    index: {
+      entry: "packages/website/src/main.js",
+      // 模板来源
+      template: "packages/website/index.html",
+      // 在 dist/index.html 的输出
+      filename: "index.html",
+      // 当使用 title 选项时，
+      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "Sandshrew",
+      // 在这个页面中包含的块，默认情况下会包含
+      // 提取出来的通用 chunk 和 vendor chunk。
+      chunks: ["chunk-vendors", "chunk-common", "index"],
     },
-    pages: {
-        index:{
-            entry: 'packages/website/src/main.js',
-            // 模板来源
-            template: 'packages/website/public/index.html',
-            // 在 dist/index.html 的输出
-            filename: 'index.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-            title: 'Sandshrew',
-            // 在这个页面中包含的块，默认情况下会包含
-            // 提取出来的通用 chunk 和 vendor chunk。
-            chunks: ['chunk-vendors', 'chunk-common', 'index']
-        }
-        
-    }
-}
+  },
+};
